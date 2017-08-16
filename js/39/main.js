a52a66c74f99af2ec591ec1d2376c30c988f313e
sunorz <sunplace@live.cn>
2017-08-16 16:22:59 +0800

    update

--- js/39/main.js
+++ js/39/main.js
@@ -1,32 +1,64 @@
 $(".codeblock").each(function(index,ele){
-		//循环代码块
-		var arr=$(this).html().split("\n");
-	    //arr.splice(arr.length-1,1);//移除最后一个空白
-		var textc = "";	//代码初始化
-		var text="<ul class='linenum'>"//行号初始化
-		var sline=startLine($(this));
+	//循环代码块
+	var arr=$(this).html().split("\n");
+
+	var textc = "";	//代码初始化
+	var sline=startLine($(this));
+	if(sline>1){
+		arr.splice(arr.length-1,1);//移除最后一个空白
+	}
+	var maxline=sline+arr.length-1;
+	$(this).css("marginLeft",maxline.toString().length+1+"em");
+	var text="<ol class='linenum' start='"+sline+"'>"//行号初始化
+
+	$(arr).each(function(index,ele){
+		//循环行
+
+		if(arr[index].trim()==""){arr[index]="&nbsp;";}
+		arr[index]='<div name="L'+sline+'">'+arr[index]+'</div>';
+		text+="<li></li>";		
+		textc+=arr[index];
+		sline++;	
+
+		});
+	
+	text+="</ol>";
+	$(this).html(textc);
+	$(this).append(text);	
+	initLine($(this));
+
+});
+
+//窗口变化
+$(window).resize(function(){
+	setHeight();
+});
+
+//行高亮
+function initLine(obj){
+	var hightline = obj.attr("line");
+	if(hightline!=null)
+	{
+		var arr = obj.attr("line").split(',');
 		$(arr).each(function(index,ele){
-			//循环行
-		
-			if(arr[index].trim()==""){arr[index]="&nbsp;";}
-				arr[index]='<div name="L'+sline+'">'+arr[index]+'</div>';
-				if(index!==arr.length-1){
-					arr[index]+="\n";
-				}	
-				text+="<li>"+sline+"</li>";		
-				textc+=arr[index];
-			sline++;
-
-			});
-		        text+="</ul>";
-				$(this).html(textc);
-		        $(this).append(text);
-		        setHeight();
-		        initLine($(this));
-				
-	});
-
-	function setHeight(){
+			obj.find("div[name=L"+ele+"]").addClass("codemark");
+		});
+	}
+}
+//指定行开始
+function startLine(obj){
+	var sline = obj.attr("start");
+	if(sline!=null)
+	{
+		return sline;
+	}
+	else
+	{
+		return 1;
+	}
+}
+//设置行号对齐
+function setHeight(){
 	$("div[name^=L]").each(function(index,ele){
 		var j=$(this).next().position().top-$(this).position().top;
 		if(j>0){
@@ -35,33 +67,6 @@ $(".codeblock").each(function(index,ele){
 	
 			});
 	$(".linenum").each(function(){
-		$(this).height($(this).parent(".codeblock").height());
+		$(this).height($(this).closest(".codeblock").height()-2);
 	})
 	}
-	
-	//行高亮
-	function initLine(obj){
-		var hightline = obj.attr("line");
-		if(hightline!=null)
-			{
-		var arr = obj.attr("line").split(',');
-		$(arr).each(function(index,ele){
-		obj.find("div[name=L"+ele+"]").addClass("mark");
-		});
-			}
-	}
-	//指定行开始
-	function startLine(obj){
-		var sline = obj.attr("start");
-		if(sline!=null)
-			{
-		       return sline;
-			}
-		else
-			{
-				return 1;
-			}
-	}
-	$(window).resize(function(){
-		setHeight();
-	});
