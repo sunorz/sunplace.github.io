b35ba11e440b89227a87cb861ba76a973199541d
sunorz <sunplace@live.cn>
2018-01-02 14:04:41 +0800

    update

---
+++ js/n2/main.js
@@ -0,0 +1,52 @@
+$(function(){
+	$("#email").bind('input propertychange', function() {	
+		ckMail($(this).val(),$(this));
+	});
+});
+//变换样式
+function setStyle(obj,code){
+		switch(code){
+			case 0:
+				obj.next("i").attr("class","fa fa-filter fa-2x form-control-feedback");
+				break;
+			case 1:
+				obj.next("i").attr("class","fa fa-check fa-2x form-control-feedback");                                                                                                                                                                                                          
+				break;
+			case -1:
+				obj.next("i").attr("class","fa fa-times fa-2x form-control-feedback");
+				break;
+			default:
+				obj.next("i").attr("class","fa fa-filter fa-2x form-control-feedback");
+				break;
+		}
+}
+//验证邮箱地址
+function ckMail(mail,obj){
+//正则表达式
+var reg = /^([\.a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/;
+//过滤（可选）
+var filter = ['qq.com','163.com','outlook.com','sina.com'];
+filter=filter.sort();
+	if($.trim(mail)==='')//为空
+	{ 
+		setStyle(obj,0);
+		return false;  
+	}	
+	else
+	{//不为空
+		$.each(filter,function(index,ele){
+			if(reg.test(mail)&&(mail.lastIndexOf(ele)===(mail.indexOf("@")+1))){
+				//符合正则且后缀符合要求
+				setStyle(obj,1);
+				return false;  		
+			}
+			else
+			{
+				setStyle(obj,-1);
+			}
+		});
+	}
+	
+	
+
+}
